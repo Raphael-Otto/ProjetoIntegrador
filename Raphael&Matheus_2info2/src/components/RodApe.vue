41c58c21f3235377257dbe41aafe3fa844d31791
Raphael <101464708+Raphael-Otto@users.noreply.github.com>
2022-05-16 09:07:05 -0300

    Add files via upload

---
+++ Raphael&Matheus_2info2/src/components/RodApe.vue
@@ -0,0 +1,12 @@
+<script>
+export default {};
+</script>
+<template>
+  <footer id="footer"></footer>
+</template>
+<style>
+#footer {
+  grid-area: footer;
+  background-color: #ffeff5;
+}
+</style>
