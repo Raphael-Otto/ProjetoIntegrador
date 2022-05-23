0ac0d2558ff6b5d2b5e5b81c5871294893ae6fe2
Raphael <101464708+Raphael-Otto@users.noreply.github.com>
2022-05-23 08:11:16 -0300

    Add files via upload

---
+++ Raphael&Matheus_2info2/Raphael&Matheus_2info2/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabeCalho from "@/components/CabeCalho.vue";
+import PrincIpal from "@/components/PrincIpal.vue";
+import RodApe from "@/components/RodApe.vue";
+export default {
+  components: { MenuLateral, CabeCalho, PrincIpal, RodApe },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabeCalho />
+  <PrincIpal />
+  <RodApe />
+</template>
+<style></style>
